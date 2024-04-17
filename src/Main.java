9bb43e735fd2dc7fcd2f4c4ebce4c25d675de52b
R1d3r90 <hubertlion@aol.com>
2024-04-17 12:17:09 +0200

    add arrays

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
